2cbdd8de9dc18f3798db27f21586c7a59f079a87
pppssshhh <alalftkdwk@gmail.com>
2024-07-10 18:47:57 +0900

    security

--- src/test/java/com/webtoon/persistence/DataSourceTests2.java
+++ src/test/java/com/webtoon/persistence/DataSourceTests2.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
