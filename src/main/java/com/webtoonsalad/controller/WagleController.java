296da01c81b13b60580a26d2c358172f708c7f0a
woodongle <315ehdgur@gmail.com>
2024-07-11 17:54:19 +0900

    WagleWagle view

--- src/main/java/com/webtoonsalad/controller/WagleController.java
+++ src/main/java/com/webtoonsalad/controller/WagleController.java
@@ -1,11 +1,28 @@
 package com.webtoonsalad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.webtoonsalad.service.WagleService;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
+@AllArgsConstructor
+@RequestMapping("/wagle/*")
 public class WagleController {
 
+	@Autowired
+	private WagleService wagleService;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", wagleService.getList());
+	}
 }
